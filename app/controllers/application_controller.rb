510161caa162ea0b60acfa0df646dcc8a2466214
Alejandro Ballesteros <alexjabf@gmail.com>
2014-03-26 13:40:54 -0700

    Cambios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_faye_url
+  rescue_from ActionController::RoutingError, :with => :render_root
   
   def set_faye_url
     session[:faye_url] = Rails.env.development? ? "http://#{IPSocket.getaddress(Socket.gethostname)}:9292/faye" : "http://www.railstogo.com:9292/faye"
@@ -13,6 +14,10 @@ class ApplicationController < ActionController::Base
       session[:locale] = params[:locale]
     end
     I18n.locale = session[:locale] || I18n.default_locale
+  end
+  
+  def render_root
+    redirect_to root_path
   end
 
 end
